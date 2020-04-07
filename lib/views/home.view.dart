8f33e5191adea824ec6e2b3c3ed110fc8d3095c0
raphael.vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2020-04-07 09:04:36 -0300

    Inserindo widget todo list

--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:todos_flutter/widgets/navbar.widget.dart';
+import 'package:todos_flutter/widgets/todo-list.widget.dart';
 import 'package:todos_flutter/widgets/user-card.widget.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        UserCard(),
-        Navbar(),
-      ],
-    ));
+      body: Column(
+        children: <Widget>[
+          UserCard(),
+          Navbar(),
+          Expanded(
+            child: Container(
+              child: TodoList(),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
   }
 }
