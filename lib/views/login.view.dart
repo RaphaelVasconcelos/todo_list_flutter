5f86d409bd5ae62b94e80441a8d93cdd01c4a6cc
raphael.vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2020-04-08 14:30:56 -0300

    Inserindo função de login com o firebase

--- lib/views/login.view.dart
+++ lib/views/login.view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todos_flutter/components/button.widget.dart';
 import 'package:todos_flutter/controllers/login.controller.dart';
 import 'package:todos_flutter/views/home.view.dart';
+import 'package:todos_flutter/widgets/busy.widget.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -53,36 +54,39 @@ class _LoginViewState extends State<LoginView> {
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(10),
-          child: busy == true
-              ? CircularProgressIndicator()
-              : Card(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        width: double.infinity,
-                      ),
-                      Image.asset(
-                        "assets/images/notification.png",
-                        width: 250,
-                      ),
-                      Text(
-                        "Olá desconhecido",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      TDButton(
-                        text: "Login com o Google",
-                        image: "assets/images/google.png",
-                        callback: () {},
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                    ],
+          child: TDBusy(
+            busy: busy,
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    width: double.infinity,
                   ),
-                ),
+                  Image.asset(
+                    "assets/images/notification.png",
+                    width: 250,
+                  ),
+                  Text(
+                    "Olá desconhecido",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  TDButton(
+                    text: "Login com o Google",
+                    image: "assets/images/google.png",
+                    callback: () {
+                      handleSignIn();
+                    },
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
